0c640025a1c8e4af3a7fc4dad41241d7de9c6fcc
fanubalti786 <fanubalti786@gmail.com>
2025-09-11 00:49:45 +0500

    update

--- client/src/pages/admin/AddBlog.jsx
+++ client/src/pages/admin/AddBlog.jsx
@@ -36,7 +36,7 @@ export default function AddBlog() {
   return (
     <form onSubmit={onSubmitHandler} className='flex-1 bg-blue-50/50 text-gray-600 overflow-scroll'>
 
-      <div className='bg-white w-full max-w-3xl p-4 md:p-10 sm:m-10 shadow rounded'>
+      <div className='bg-white max-w-3xl p-4 md:p-10 m-1 sm:m-10 shadow rounded border'>
 
         <p>Upload thumbnail</p>
         <label htmlFor="image" className='inline-flex'>
@@ -52,15 +52,14 @@ export default function AddBlog() {
 
         <p className='mt-4'>Sub title</p>
         <input type="text" placeholder='Type here' required value={subTitle}
-        onChange={(e) => setTitle(e.target.value)}
+        onChange={(e) => setSubTitle(e.target.value)}
         className='w-full max-w-lg mt-2 p-2 border border-gray-300 outline-none rounded' />
 
         <p className='mt-4'>Blog Description</p>
-        <div className='max-w-lg h-74 pb-16 sm:pb-10 pt-2 relative  '>
-
-          <div ref={editorRef}></div>
+        <div className='max-w-lg h-74 pb-16 sm:pb-10 relative'>
+          <div ref={editorRef} className=''></div>
           <button onClick={generateContent} type='button' 
-          className='absolute bottom-1 right-2 text-xs text-white
+          className='absolute bottom-1 right-2 text-xs text-white 
         bg-black/70 px-4 py-1.5 rounded hover:underline cursor-pointer'>Generate with AI</button>
 
         </div>
@@ -70,7 +69,7 @@ export default function AddBlog() {
           className='mt-2 px-3 py-2 border text-gray-500 border-gray-300 outline-none rounded'>
           <option value="">Select category</option>
           {blogCategories.map((item,index) => {
-            return <option value="item">{item}</option>
+            return <option value={item}>{item}</option>
           })}
         </select>
 
